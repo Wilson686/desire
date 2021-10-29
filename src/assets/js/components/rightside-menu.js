a4c1adeca9a07e7542461d502bca7588ef63b60d
Wilson686 <sheleminmaksim2000@gmail.com>
2021-10-29 16:20:35 +0300

    сделаные небольшие правки и польность завершена главная страница

--- src/assets/js/components/rightside-menu.js
+++ src/assets/js/components/rightside-menu.js
@@ -4,9 +4,11 @@ const rightsideMenu = $("#rightside-menu");
 const rightsideMenuBtn = $("#rightside-menu__close");
 
 headerBtn.on("click", function() {
-    rightsideMenu.removeClass("rightside-menu--closed")
+    rightsideMenu.removeClass("rightside-menu--closed");
+    $('body').addClass("no-scroll");
 });
 
 rightsideMenuBtn.on("click", function() {
-    rightsideMenu.addClass("rightside-menu--closed")
+    rightsideMenu.addClass("rightside-menu--closed");
+    $('body').removeClass("no-scroll");
 });
